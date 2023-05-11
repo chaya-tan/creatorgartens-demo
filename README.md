2e9e0c3fa8b184192b59fd4c5d4e8a4b061eda22
chaya-tan <chayatan.ang@gmail.com>
2023-05-11 11:32:27 +0700

    added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# The fp-ts problem
+
+This repo is made for Creatorkarten talk on May 19th, 2023
+
+## to run
+
+```sh
+npm run run
+```
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var option_1 = require("fp-ts/option");
 var bob = {
     name: "Bob",
-    eyeCount: 2
+    eyeCount: 2,
 };
 var dave = {
     name: "Dave",
-    eyeCount: 2
+    eyeCount: 2,
 };
 var tRexBoss = {
     name: "Rex",
-    isAlive: false
+    isAlive: false,
 };
 var mayBeBob = (0, option_1.some)(bob);
 var mayBeTRex = (0, option_1.some)(tRexBoss);
-console.log(bob === dave); // this should
-// console.log(bob === tRexBoss);
-console.log(mayBeBob === mayBeTRex); // not alarm "no overlap"
+console.log(bob === dave); // this should be comparable
+// console.log(bob === tRexBoss); // this should be error
+console.log(mayBeBob === mayBeTRex); // this should be error, but nooo
